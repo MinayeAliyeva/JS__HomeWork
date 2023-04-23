b07a7d2fc93228fb030dd2700e2c16863eca744d
MinayeAliyeva <7s346pu@code.edu.az>
2023-04-23 12:01:04 +0400

    added Dom vs Bom task

---
+++ 18.Dom vs Bom-first-task/main.js
@@ -0,0 +1,28 @@
+let startBtn = document.querySelector(".start");
+let result = document.querySelector(".result");
+let stopBtn = document.querySelector(".stop");
+let increase = document.querySelector("#increase");
+let declaretment = document.querySelector("#declerement");
+let reset = document.querySelector("#reset");
+let count = 0;
+let interval;
+startBtn.addEventListener("click", function () {
+  interval = setInterval(() => {
+    ++count;
+    result.innerText = count;
+  }, 1000);
+});
+stopBtn.addEventListener("click", function () {
+  clearInterval(interval);
+});
+increase.addEventListener("click", function () {
+  result.innerText = ++count;
+});
+declaretment.addEventListener("click", function () {
+  result.innerText = --count;
+});
+reset.addEventListener("click", function () {
+  clearInterval(interval);
+  count = 0;
+  result.innerText = count;
+});
